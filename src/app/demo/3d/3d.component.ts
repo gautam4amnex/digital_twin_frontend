432772f5c91aa4f4486de0d4097afcd6980526b2
Darshan Maniyar <darshan.maniyar@ispl.com>
2024-04-03 12:35:48 +0530

    3D layer Panel

--- src/app/demo/3d/3d.component.ts
+++ src/app/demo/3d/3d.component.ts
@@ -5,6 +5,8 @@ import * as Cesium from 'cesium';
 import { CesiumDirective } from './cesium.directive';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-3d',
@@ -12,10 +14,44 @@ import { CommonModule } from '@angular/common';
   //imports: [SharedModule],
   templateUrl: './3d.component.html',
   styleUrls: ['./3d.component.scss'],
-  imports: [CesiumDirective,RouterOutlet,CommonModule]
+  imports: [CesiumDirective, RouterOutlet, CommonModule]
   //template: '<div appCesium></div>'
 })
+
 export default class _3D {
-  
+  data: any;
+
+  get_layer_panel_data() {
+
+    var formData = { 
+      "page_name": "3D" 
+    }
+
+    axios({
+
+      url: "http://localhost:8085/get_all_layer_and_image",
+      method: "POST",
+      data: formData,
+    })
+
+      .then((res) => {
+        if(res.data.responseCode == 200){
+          debugger
+          console.log(res.data.data);
+          this.data = res.data.data;
+        }
+       })
+
+
+      .catch((err) => { });
+
+  }
+
+
+  ngOnInit() {
+
+    this.get_layer_panel_data();
+
+  }
 
 }
