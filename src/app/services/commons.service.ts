10c6851d6ef923580580fc4333e218d053568484
Darshan Maniyar <darshan.maniyar@ispl.com>
2024-05-17 10:17:36 +0530

    conflict resolved

--- src/app/services/commons.service.ts
+++ src/app/services/commons.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import * as glob from '../../environments/environment';
 import { BehaviorSubject } from 'rxjs';
-import { Role } from '../models/role';
+import { Role } from '../models/role'; 
 import { MatSnackBar } from '@angular/material/snack-bar';
  import { Observable } from 'rxjs';
 
