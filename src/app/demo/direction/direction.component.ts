6112252df638731e06d5a9fd50450a928a312bc5
Hinal Panchal <hinal.panchal@ispl.com>
2024-07-09 18:42:32 +0530

    design done in directions

--- src/app/demo/direction/direction.component.ts
+++ src/app/demo/direction/direction.component.ts
@@ -12,7 +12,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-direction',
   standalone: true,
-  imports: [CommonModule,HttpClientModule,HttpClientModule],
+  imports: [CommonModule, HttpClientModule, HttpClientModule],
   templateUrl: './direction.component.html',
   styleUrls: ['./direction.component.scss']
 })
@@ -38,17 +38,24 @@ export class DirectionComponent implements OnInit {
   start: any[];
   end: any[];
   current_coords: number[];
-  picked_coords:any[];
+  picked_coords: any[];
 
-  source_element=document.getElementById('input_start');
-  destination_element=document.getElementById('input_end');
+  source_element = document.getElementById('input_start');
+  destination_element = document.getElementById('input_end');
   isSourcePickActive: boolean = false;
   isDestinationPickActive: boolean = false;
-  inputValueSource: any[] =['empty'] ;
-  inputValueDestination:any[]=['empty'];
+  inputValueSource: any[] = ['empty'];
+  inputValueDestination: any[] = ['empty'];
+  overlay: any;
+  container = document.getElementById('popup');
+  content = document.getElementById('popup-content');
+  closer = document.getElementById('popup-closer');
   
-  constructor(private http: HttpClient) {}
-  ngOnInit(): void {
+  routeSteps: any;
+  startAddress: any;
+  endAddress: any;
+
+  constructor(private http: HttpClient) {
     this.osm = new this.ol.layer.Tile({
       source: new this.ol.source.OSM()
     });
@@ -59,11 +66,6 @@ export class DirectionComponent implements OnInit {
       zoom: 12
     });
 
-    this.map = new this.ol.Map({
-      layers: [this.osm],
-      target: 'map',
-      view: this.view
-    });
     this.pointStyle = new this.ol.style.Style({
       image: new this.ol.style.Icon({
         anchor: [0.5, 1],
@@ -71,155 +73,223 @@ export class DirectionComponent implements OnInit {
       })
     });
 
-    this.map.addLayer(this.vector);
+
+    this.overlay = new this.ol.Overlay({
+      element: this.container,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250
+      }
+    });
+
   }
 
-  getDirections(start: number[], end: number[]): void {
-
-    this.http.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624837d0d92fba7048cd85a8893a7148bfdb&start=${start}&end=${end}`).subscribe(
-      (data)=>{
-     
-      let features = new this.ol.format.GeoJSON().readFeatures(data, {
-        featureProjection: 'EPSG:3857'
-      });
-      let extent = this.ol.extent.createEmpty();
-      features.forEach((feature: any) => {
-      this.ol.extent.extend(extent, feature.getGeometry().getExtent());
+  ngOnInit(): void {
+    this.map = new this.ol.Map({
+      layers: [this.osm],
+      target: 'map',
+      view: this.view
     });
+    this.map.addOverlay(this.overlay);
+    this.map.addLayer(this.vector);
+    this.closer.onclick = () => {
+      this.overlay.setPosition(undefined);
+      this.closer.blur();
+      return false;
+    };
+
+    this.map.on('singleclick', (event) => {
+      this.map.forEachFeatureAtPixel(event.pixel, (feature, layer) => {
+        const coordinates = feature.getGeometry().getCoordinates();
+        const content = document.getElementById('popup-content');
+        content.innerHTML = `<table>
+          <tr><th>Instruction</th><td>${feature.get('instruction')}</td></tr>
+          <tr><th>Address</th><td>${feature.get('name')}</td></tr>
+        </table>`;
+        this.overlay.setPosition(coordinates);
+      });
+    });
+ ;
+ 
+
+
+  }
+  getDirections(start: number[], end: number[]): void {
+    this.http.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624837d0d92fba7048cd85a8893a7148bfdb&start=${start}&end=${end}`)
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          let features = new GeoJSON().readFeatures(data, {
+            featureProjection: 'EPSG:3857'
+          });
+
+          let extent = this.ol.extent.createEmpty();
+          features.forEach((feature: any) => {
+            this.ol.extent.extend(extent, feature.getGeometry().getExtent());
+          });
+
+          this.map.getView().fit(extent, { padding: [50, 50, 50, 50] });
+          this.source.addFeatures(features);
 debugger;
-   // Extract coordinates from the JSON response
-  //  const segment =data.features[0].properties.segments;
-  //  const coordinates =data.features[0].geometry.properties.coordinates;
-  
- // Add intermediate markers
-//  for (let i = 1; i < coordinates.length - 1; i++) {
-//   this.addMarker(coordinates[i]);
-
-// }
-this.map.getView().fit(extent, { duration: 1500,  maxZoom: 14 });
-      this.source.addFeatures(features);
-
-    },
-  (error)=>{
-    alert('cant find route');
-  });
+          const segments = data.features[0].properties.segments;
+          if (segments && segments.length > 0) {
+            const steps = segments[0].steps;
+            if (steps && steps.length > 0) {
+              steps.forEach((step: any) => {
+                this.routeSteps = steps;
+                step.way_points.forEach((wpIndex: number) => {
+                  const coordinate = data.features[0].geometry.coordinates[wpIndex];
+                  this.addMarker(coordinate, step.instruction, step.name);
+                  console.log('coordinate, step.instruction, step.name',coordinate, step.instruction, step.name)
+                 
+                });
+              });
+            } else {
+              console.error('No steps found in the segments');
+            }
+          } else {
+            console.error('No segments found in the features');
+          }
+        },
+        (error: any) => {
+          alert('cant find route');
+        }
+      );
   }
 
   findCoords(evt: any) {
-  
-  if(this.isSourcePickActive){
-    debugger
-    const coordinate = this.map.getEventCoordinate(evt);
-    const transformedCoordinate = this.ol.proj.toLonLat(coordinate);
-    this.picked_coords=transformedCoordinate;
-    console.log(this.picked_coords);
-    
-    this.start=this.picked_coords
-     console.log('start',this.start);
-     this.inputValueSource=this.start;
-     this.addMarker(this.start);
-    this.isSourcePickActive = false;
-    this.picked_coords=[];
-  }
-  if (this.isDestinationPickActive) {
 
-    const coordinate = this.map.getEventCoordinate(evt);
-    console.log('Event coordinate:', coordinate);
-  
-    const transformedCoordinate = this.ol.proj.toLonLat(coordinate);
-    console.log('Transformed coordinate:', transformedCoordinate);
+    if (this.isSourcePickActive) {
+      debugger
+      const coordinate = this.map.getEventCoordinate(evt);
+      this.picked_coords= this.ol.proj.toLonLat(coordinate);
 
-    this.picked_coords=transformedCoordinate;
-    console.log(this.picked_coords);
-    this.end=this.picked_coords
-      console.log('end',this.end);
-      
-      this.inputValueDestination=this.end;
-      this.addMarker(this.end);
+      this.start = this.picked_coords;
+      console.log('start', this.start);
+      this.getAddress(this.start, 'start')
+      this.inputValueSource = this.start;
+      this.addMarker(this.start, 'start', 'hhh');
+      this.isSourcePickActive = false;
+      this.picked_coords = [];
+    }
+    if (this.isDestinationPickActive) {
+
+      const coordinate = this.map.getEventCoordinate(evt);
+      console.log('Event coordinate:', coordinate);
+
+      const transformedCoordinate = this.ol.proj.toLonLat(coordinate);
+      console.log('Transformed coordinate:', transformedCoordinate);
+
+      this.picked_coords = transformedCoordinate;
+      console.log(this.picked_coords);
+      this.end = this.picked_coords
+      console.log('end', this.end);
+      this.getAddress(this.end, 'end');
+      this.inputValueDestination = this.end;
+      this.addMarker(this.end, 'destionation', 'nn');
       this.isDestinationPickActive = false;
-      this.picked_coords=[];
-    
-  } 
-     
-  
+      this.picked_coords = [];
 
- 
+    }
+
+
+
+
+  }
+  addMarker(geo_coords: any[], instruction: string, name: string): void {
+    const markerFeature = new this.ol.Feature({
+      geometry: new this.ol.geom.Point(this.ol.proj.fromLonLat(geo_coords)),
+      instruction: instruction,
+      name: name
+    });
+
+    markerFeature.setStyle(this.pointStyle);
+    this.source.addFeature(markerFeature);
   }
 
-addMarker(geo_coords){
+  // addMarker(geo_coords){
 
-  let addMarkserFeature = new this.ol.Feature({
-    geometry: new this.ol.geom.Point(this.ol.proj.fromLonLat(geo_coords)),
-    featureProjection: 'EPSG:3826',
-});
-
- 
-addMarkserFeature.setStyle(this.pointStyle);
-this.source.addFeature(addMarkserFeature);
-
-}
+  //   let addMarkserFeature = new this.ol.Feature({
+  //     geometry: new this.ol.geom.Point(this.ol.proj.fromLonLat(geo_coords)),
+  //     featureProjection: 'EPSG:3826',
+  // });
 
 
-async pickLocation(value:any,event:any) {
-    
-    if(value=='start')
-    {
+  // addMarkserFeature.setStyle(this.pointStyle);
+  // this.source.addFeature(addMarkserFeature);
+
+  // }
+
+
+  async pickLocation(value: any, event: any) {
+
+    if (value == 'start') {
       this.isSourcePickActive = true;
 
     }
-    else{
+    else {
       this.isDestinationPickActive = true;
-  
+
     }
-   
+
   }
 
- 
 
-  getCurrentLocation(value:any): void {
+
+  getCurrentLocation(value: any): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         this.current_coords = [position.coords.longitude, position.coords.latitude];
-        console.log('coords',this.current_coords);
-        this.addMarker(this.current_coords);
-        if(value=='start'){
-          this.start=this.current_coords;
-          console.log('start',this.start);
-          this.inputValueSource=this.start;
-   
-        }
-        else{
+        console.log('coords', this.current_coords);
+        this.addMarker(this.current_coords, 'u r here', 'place');
+        if (value == 'start') {
+          this.start = this.current_coords;
+          console.log('start', this.start);
+          this.inputValueSource = this.start;
 
-          this.end=this.current_coords;
-          console.log('end',this.end);
-          this.inputValueDestination=this.end;
-  
+        }
+        else {
+
+          this.end = this.current_coords;
+          console.log('end', this.end);
+          this.inputValueDestination = this.end;
+
         }
 
       });
     }
-     else {
+    else {
       alert('Geolocation is not supported by your browser.');
     }
   }
 
-  findRoute(){
-    if(this.start!=null && this.end!=null){
-      console.log('this.start, this.end',this.start, this.end)
+  findRoute() {
+    if (this.start != null && this.end != null) {
+      console.log('this.start, this.end', this.start, this.end)
       this.getDirections(this.start, this.end);
-    //   this.inputValueSource=['empty'];
-    // this.inputValueSource=['empty'];
+
     }
-    else{
+    else {
       alert('enter both coords');
     }
-   
-    
+
+
   }
-  clear(){
-    this.inputValueSource=['empty'];
-     this.inputValueDestination=['empty'];
-    this.source.clear(); 
-   
+  clear() {
+    this.inputValueSource = ['empty'];
+    this.inputValueDestination = ['empty'];
+    this.source.clear();
+
+  }
+  getAddress(coords: number[], type: string): void {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords[1]}&lon=${coords[0]}&addressdetails=1`;
+    this.http.get(url).subscribe((response: any) => {
+      const address = response.display_name;
+      if (type === 'start') {
+        this.startAddress = address;
+      } else if (type === 'end') {
+        this.endAddress = address;
+      }
+    });
   }
 }
